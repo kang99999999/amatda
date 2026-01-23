9f4fbee5a5cc3452e6398fc7273c4bd7417b066b
kang99999999 <imshinzle@naver.com>
2026-01-23 12:54:51 +0900

    pwa update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,3 +29,10 @@ export default function RootLayout({
     </html>
   );
 }
+
+export const metadata = {
+  title: "아 맞다!",
+  description: "원터치 점검으로 아맞다!는 이제 그만~",
+  manifest: "/manifest.json",
+  themeColor: "#4A90E2",
+};
